5f6f7458b66d6a396f49d153e6f494dc32dab2d1
Chinmay Nivsarkar <mit.chinmayn@gmail.com>
2017-04-05 20:10:43 +0530

    Splash screen

--- app/src/main/java/com/example/probook33/ignaro/SplashScreen.java
+++ app/src/main/java/com/example/probook33/ignaro/SplashScreen.java
@@ -1,13 +1,31 @@
 package com.example.probook33.ignaro;
 
+import android.app.Activity;
+import android.content.Intent;
+import android.os.Handler;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
-public class SplashScreen extends AppCompatActivity {
+import java.util.Timer;
+import java.util.TimerTask;
+
+public class SplashScreen extends Activity {
+
+    private static int SPLASH_TIME_OUT = 3000;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_splash_screen);
+
+        new Timer().schedule(new TimerTask() {
+            @Override
+            public void run() {
+                // this code will be executed after 2 seconds
+                Intent i = new Intent(SplashScreen.this, Register.class);
+                startActivity(i);
+            }
+        }, 2000);
+
     }
 }
